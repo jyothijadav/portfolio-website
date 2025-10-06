7003cdd5e9f1ff96855b67ff471f7f8772945dac
Jyothi Jadav <jyothijadav913@gmail.com>
2025-10-06 23:31:31 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-    ,tailwindcss(),
+  base: '/portfolio-website/',  // ← ADD THIS LINE
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
